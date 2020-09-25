a2cb1fc0e2052a0ca5ed729df5ceb122906fb73e
Adrian <reneavila1993@gmail.com>
2020-09-24 22:02:58 -0600

    Disable components while applying RWD styling

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <div id="app">
-    <SideNavigator />
+    <!-- <SideNavigator /> -->
     <Hero />
-    <About />
-    <Portfolio /> 
-    <Blogs /> 
-    <Contact />
+    <!-- <About /> -->
+    <!-- <Portfolio />  -->
+    <!-- <Blogs />  -->
+    <!-- <Contact /> -->
   </div>
 </template>
 
 <script>
 import Hero from './components/Hero';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Blogs from './components/Blogs';
-import Contact from './components/Contact';
-import SideNavigator from './components/SideNavigator';
+// import About from './components/About';
+// import Portfolio from './components/Portfolio';
+// import Blogs from './components/Blogs';
+// import Contact from './components/Contact';
+// import SideNavigator from './components/SideNavigator';
 
 export default {
   name: 'app',
   components: {
     Hero,
-    About,
-    Portfolio,
-    Blogs,
-    Contact,
-    SideNavigator,
+    // About,
+    // Portfolio,
+    // Blogs,
+    // Contact,
+    // SideNavigator,
   }
 }
 </script>
 
 <style>
+
 * {
   margin: 0;
   padding: 0;
@@ -41,13 +42,14 @@ html {
   font-size: 10px;
   font-family: 'Roboto', sans-serif;
   color: #eee;
+  max-width: 100%;
 }
 
 body {
-  width: 100%;
+  max-width: 100%;
   height: 100%;
   background: url('./assets/images/background.jpg') no-repeat center center fixed;
-  background-size: cover;
+  background-size: 100% 100%;
 }
 
 a {
@@ -56,7 +58,7 @@ a {
 }
 
 p {
-  font-size: 1.8rem;
+  /* font-size: 1.8rem; */
   font-weight: 300;
 }
 
@@ -64,41 +66,5 @@ img {
   width: 100%;
 }
 
-/* For mobile phones: */
-[class*="col-"] {
-  width: 100%;
-}
-
-@media only screen and (min-width: 600px) {
-  /* For tablets: */
-  .col-s-1 {width: 8.33%;}
-  .col-s-2 {width: 16.66%;}
-  .col-s-3 {width: 25%;}
-  .col-s-4 {width: 33.33%;}
-  .col-s-5 {width: 41.66%;}
-  .col-s-6 {width: 50%;}
-  .col-s-7 {width: 58.33%;}
-  .col-s-8 {width: 66.66%;}
-  .col-s-9 {width: 75%;}
-  .col-s-10 {width: 83.33%;}
-  .col-s-11 {width: 91.66%;}
-  .col-s-12 {width: 100%;}
-}
-
-@media only screen and (min-width: 992px) {
-  /* For desktop: */
-  .col-1 {width: 8.33%;}
-  .col-2 {width: 16.66%;}
-  .col-3 {width: 25%;}
-  .col-4 {width: 33.33%;}
-  .col-5 {width: 41.66%;}
-  .col-6 {width: 50%;}
-  .col-7 {width: 58.33%;}
-  .col-8 {width: 66.66%;}
-  .col-9 {width: 75%;}
-  .col-10 {width: 83.33%;}
-  .col-11 {width: 91.66%;}
-  .col-12 {width: 100%;}
-}
 
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="banner-page">
-        <div class="banner-text">
+    <div class="row banner-page">
+        <div class="col-12 banner-text">
             <h1>Adrian Avila</h1>
             <h6>Full Stack Software Engineer</h6>
         </div>
@@ -14,20 +14,54 @@ export default {
 </script>
 
 <style scoped>
-.banner-page {
+
+/* For mobile phones: */
+@media only screen and (max-width: 600px) {
+    [class*="col-"] {
     width: 100%;
+    }
+
+    .banner-text h1 {
+        font-size: 6rem;
+    }
+
+    .banner-text h6 {
+        font-size: 1.75rem;
+    }
+}
+
+@media only screen and (min-width: 768px) {
+  /* For desktop: */
+  .col-1 {width: 8.33%;}
+  .col-2 {width: 16.66%;}
+  .col-3 {width: 25%;}
+  .col-4 {width: 33.33%;}
+  .col-5 {width: 41.66%;}
+  .col-6 {width: 50%;}
+  .col-7 {width: 58.33%;}
+  .col-8 {width: 66.66%;}
+  .col-9 {width: 75%;}
+  .col-10 {width: 83.33%;}
+  .col-11 {width: 91.66%;}
+  .col-12 {width: 100%;}
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.banner-page {
+    display: flex;
+    align-items: center;
     height: 100vh;
 }
 
+
 .banner-text {
     font-family: 'Permanent Marker', cursive;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate( -50%, -50%);
-    width: 100%;
     text-align: center;
-    user-select: none;
 }
 
 h1 {
