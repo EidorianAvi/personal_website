21e877fd5e80e741cd86bbe92c93fedd16f23a72
Adrian <reneavila1993@gmail.com>
2020-09-27 16:35:44 -0600

    Get expanding demo descriptions to work with expansion and retraction

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -13,21 +13,21 @@
                     <div class="iframe-container">
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/aZMOCxSa3Yg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                    <p>smallTalk is the middleman to interest and hobby driven conversation. Sometimes it's difficult to  <span class="see-more" @click="expandDescription(1)">...see more</span><span class="more">spark up a conversation about topics that are near and dear to you but have nobody to relate with. That's where smallTalk comes in it's the one stop shop to meet, converse, and connect with likeminded individuals. Technologies used were: Flutter, Dart, Firebase, and Firestore. </span><span class="see-less">...see less</span></p>
+                    <p>smallTalk is the middleman to interest and hobby driven conversation. Sometimes it's difficult to  <span class="see-more-1" @click="expandDescription(1)">...see more</span><span class="more-1">spark up a conversation about topics that are near and dear to you but have nobody to relate with. That's where smallTalk comes in it's the one stop shop to meet, converse, and connect with likeminded individuals. Technologies used were: Flutter, Dart, Firebase, and Firestore. </span><span class="see-less-1" @click="expandDescription(1)">...see less</span></p>
                 </div>
                 <div class="portfolio-item">
                     <h2>theRack</h2>
                     <div class="iframe-container">
                         <iframe width="560"  height="315" src="https://www.youtube.com/embed/5GngnwWPQiI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                    <p>If you're anything like me you love a good pair of sneakers but have a hard time picking out the perfect<span class="see-more" @click="expandDescription(2)">...see more</span><span class="more">pair and that's what theRack was created for. Select from hundreds of shoes in the catalogue and narrow it down to the perfect pair. Technologies used were: React, JavaScript, Ruby on Rails, and JWT.</span><span class="see-less">...see less</span></p>
+                    <p>If you're anything like me you love a good pair of sneakers but have a hard time picking out the perfect <span class="see-more-2" @click="expandDescription(2)">...see more</span><span class="more-2">pair and that's what theRack was created for. Select from hundreds of shoes in the catalogue and narrow it down to the perfect pair. Technologies used were: React, JavaScript, Ruby on Rails, and JWT.</span><span class="see-less-2" @click="expandDescription(2)">...see less</span></p>
                 </div>
                 <div class="portfolio-item">
                     <h2>Jedi Archives</h2>
                     <div class="iframe-container">
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/XglSvmMSHPk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                    <p>A common problem we all face is that our Star Wars collection is just getting larger by the day and it's<span class="see-more" @click="expandDescription(3)">...see more</span><span class="more">getting difficult to keep track. The Jedi Archives were created to have one place to store all Novels and Graphic Novels for collection management. Technologies used were: JavaScript, Ruby on Rails, CSS3, HTML5.</span><span class="see-less">...see less</span></p>
+                    <p>A common problem we all face is that our Star Wars collection is just getting larger by the day and it's <span class="see-more-3" @click="expandDescription(3)">...see more</span><span class="more-3">getting difficult to keep track. The Jedi Archives were created to have one place to store all Novels and Graphic Novels for collection management. Technologies used were: JavaScript, Ruby on Rails, CSS3, HTML5.</span><span class="see-less-3" @click="expandDescription(3)">...see less</span></p>
                 </div>
             </div>
         </div>
@@ -39,15 +39,19 @@
 export default {
     methods: {
         expandDescription(number) {
-            number;
-            var dots = document.querySelectorAll(".dots");
-            var moreText = document.querySelectorAll(".more");
-            if (dots.style.display === "none") {
-                dots.style.display = "inline";
+            
+            let seeMore = document.querySelector(`.see-more-${number}`);
+            let moreText = document.querySelector(`.more-${number}`);
+            let seeLess = document.querySelector(`.see-less-${number}`);
+
+            if (seeMore.style.display === "none") {
+                seeMore.style.display = "inline";
                 moreText.style.display = "none";
+                seeLess.style.display = "none";
            } else {
-                dots.style.display = "none";
+                seeMore.style.display = "none";
                 moreText.style.display = "inline";
+                seeLess.style.display = "inline";
             }
         },
     }
@@ -61,6 +65,12 @@ export default {
 @media only screen and (max-width: 600px) {
     [class*="col-"] {
     width: 100%;
+    }
+
+    .portfolio {
+        margin-top: 8rem;
+        max-width: 100%;
+        height: 110vh;
     }
 
     .section-heading h1 {
@@ -110,19 +120,20 @@ export default {
         font-size: 1.3rem;
     }
 
-    .see-more {
-        display: show;
+    .see-more-1, .see-more-2, .see-more-3 {
+        display: inline;
         color: grey;
         font-weight: 600;
     }
 
-    .see-less {
+    .more-1, .more-2, .more-3 {
         display: none;
     }
 
-
-    .more {
+    .see-less-1, .see-less-2, .see-less-3 {
         display: none;
+        color: grey;
+        font-weight: 600;
     }
 
     .border {
@@ -162,12 +173,7 @@ export default {
   display: table;
 }
 
-.portfolio {
-    margin-top: 8rem;
-    /* margin-bottom: 10rem; */
-    max-width: 100%;
-    height: 110vh;
-}
+
 
 
 
