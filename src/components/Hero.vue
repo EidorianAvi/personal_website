02fa2261a9b6288755f00ea6937a9a68a873af24
Adrian <reneavila1993@gmail.com>
2020-09-28 15:40:34 -0600

    Add animated text typing to hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,6 +3,11 @@
         <div class="col-12 banner-text">
             <h1>Adrian Avila</h1>
             <h6>Full Stack Software Engineer</h6>
+            <div class="css-typing">
+              <p>Star Wars Historian 🌌</p>
+              <p>Shiba Dad 🐕</p>
+              <p>Ramen Connoisseur 🍜</p>
+            </div>
         </div>
     </div>
 </template>
@@ -47,8 +52,8 @@ export default {
         background: rgb(232,232,236);
         /* background: linear-gradient(0deg, rgba(232,232,236,1) 0%, rgba(30,30,32,1) 100%); */
         background: linear-gradient(0deg, #93a5cf  0%,  #e4efe9  100%);
-        -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
+        -webkit-background-clip: text;
     }
 
     h6 { 
@@ -56,7 +61,12 @@ export default {
         font-weight: 300;
     }
 
-    
+    .css-typing {
+      margin-top: 2rem;
+      margin-left: 9rem;
+    }
+
+
 }
 
 @media only screen and (min-width: 768px) {
@@ -81,5 +91,134 @@ export default {
   display: table;
 }
 
+.css-typing p {
+  border-right: .15em solid lightgray;
+  font-size: 1.3rem;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.css-typing p:nth-child(1) {
+  width: 13em;
+  -webkit-animation: type 2s steps(40, end);
+  animation: type 2s steps(40, end);
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+.css-typing p:nth-child(2) {
+  width: 11.5em;
+  opacity: 0;
+  -webkit-animation: type2 2s steps(40, end);
+  animation: type2 2s steps(40, end);
+  -webkit-animation-delay: 2s;
+  animation-delay: 2s;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+.css-typing p:nth-child(3) {
+  width: 13em;
+  opacity:  0;
+  -webkit-animation: type3 5s steps(20, end), blink .5s step-end infinite alternate;
+  animation: type3 5s steps(20, end), blink .5s step-end infinite alternate;
+  -webkit-animation-delay: 4s;
+  animation-delay: 4s;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+/* Animations */
+
+@keyframes type {
+  0% {
+    width: 0;
+  }
+  99.9% {
+    border-right: .15em solid orange;
+  }
+  100% {
+    border: none;
+  }
+}
+
+@-webkit-keyframes type {
+  0% {
+    width: 0;
+  }
+  99.9% {
+    border-right: .15em solid orange;
+  }
+  100% {
+    border: none;
+  }
+}
+
+@keyframes type2 {
+  0% {
+    width: 0;
+  }
+  1% {
+    opacity: 1;
+  }
+  99.9% {
+    border-right: .15em solid orange;
+  }
+  100% {
+    opacity: 1;
+    border: none;
+  }
+}
+
+@-webkit-keyframes type2 {
+  0% {
+    width: 0;
+  }
+  1% {
+    opacity: 1;
+  }
+  99.9% {
+    border-right: .15em solid orange;
+  }
+  100% {
+    opacity: 1;
+    border: none;
+  }
+}
+
+@keyframes type3 {
+  0% {
+    width: 0;
+  }
+  1% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes type3 {
+  0% {
+    width: 0;
+  }
+  1% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes blink {
+  50% {
+    border-color: transparent;
+  }
+}
+@-webkit-keyframes blink {
+  50% {
+    border-color: tranparent;
+  }
+}
 
 </style>
